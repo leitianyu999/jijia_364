a89d0ca12512f91879c47787658905aae4076d55
leitianyu999 <leitianyu999@gmail.com>
2024-06-21 07:59:24 +0000

    refactor():页面重构

--- leitianyu-modules/leitianyu-job/src/main/java/com/jijia/job/task/RyTask.java
+++ leitianyu-modules/leitianyu-job/src/main/java/com/jijia/job/task/RyTask.java
@@ -1,5 +1,14 @@
 package com.jijia.job.task;
 
+import com.jijia.common.core.constant.HttpStatus;
+import com.jijia.common.core.constant.SecurityConstants;
+import com.jijia.common.core.exception.job.TaskException;
+import com.jijia.common.core.utils.SpringUtils;
+import com.jijia.common.core.web.domain.AjaxResult;
+import com.jijia.system.api.RemoteReadyDeskService;
+import com.jijia.system.api.factory.RemoteUserFallbackFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import com.jijia.common.core.utils.StringUtils;
 
@@ -11,6 +20,8 @@ import com.jijia.common.core.utils.StringUtils;
 @Component("ryTask")
 public class RyTask
 {
+    private static final Logger log = LoggerFactory.getLogger(RemoteUserFallbackFactory.class);
+
     public void ryMultipleParams(String s, Boolean b, Long l, Double d, Integer i)
     {
         System.out.println(StringUtils.format("执行多参方法： 字符串类型{}，布尔类型{}，长整型{}，浮点型{}，整形{}", s, b, l, d, i));
@@ -24,5 +35,15 @@ public class RyTask
     public void ryNoParams()
     {
         System.out.println("执行无参方法");
+    }
+
+    public void addReadyDeskToDesk() throws TaskException {
+        AjaxResult ajaxResult = SpringUtils.getBean(RemoteReadyDeskService.class).addReadyToDesk(SecurityConstants.INNER);
+        if (ajaxResult.get(AjaxResult.CODE_TAG).equals(HttpStatus.WARN)) {
+            throw new TaskException("今日台账无数据", TaskException.Code.TASK_EXISTS);
+        }
+        if (ajaxResult.isError()) {
+            throw new TaskException("台账导入失败", TaskException.Code.UNKNOWN);
+        }
     }
 }
