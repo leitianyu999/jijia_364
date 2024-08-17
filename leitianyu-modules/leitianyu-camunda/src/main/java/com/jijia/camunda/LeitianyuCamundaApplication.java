124641a4e7ab3e08bb4ddaad0443b411717d8ac8
leitianyu999 <leitianyu999@gmail.com>
2024-08-17 16:22:37 +0000

    feat(camunda):工作流模块初始化

---
+++ leitianyu-modules/leitianyu-camunda/src/main/java/com/jijia/camunda/LeitianyuCamundaApplication.java
@@ -0,0 +1,36 @@
+package com.jijia.camunda;
+
+
+import com.jijia.common.security.annotation.EnableCustomConfig;
+import com.jijia.common.security.annotation.EnableRyFeignClients;
+import com.jijia.common.swagger.annotation.EnableCustomSwagger2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 业务模块
+ *
+ * @author leitianyu
+ */
+@EnableCustomConfig
+@EnableCustomSwagger2
+@EnableRyFeignClients
+@SpringBootApplication
+public class LeitianyuCamundaApplication {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(LeitianyuCamundaApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  工作流模块启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                " .-------.       ____     __        \n" +
+                " |  _ _   \\      \\   \\   /  /    \n" +
+                " | ( ' )  |       \\  _. /  '       \n" +
+                " |(_ o _) /        _( )_ .'         \n" +
+                " | (_,_).' __  ___(_ o _)'          \n" +
+                " |  |\\ \\  |  ||   |(_,_)'         \n" +
+                " |  | \\ `'   /|   `-'  /           \n" +
+                " |  |  \\    /  \\      /           \n" +
+                " ''-'   `'-'    `-..-'              ");
+    }
+
+}
